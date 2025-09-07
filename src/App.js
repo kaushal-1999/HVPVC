5252f30d6ef623acb997d3a8679dcaad40847aff
Kaushal Patil <kaushalpatil.tech@gmail.com>
2025-09-07 10:56:47 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 import Contact from './pages/contact/Contact';
 import About from './pages/about/AboutPage';
 import Departments from './pages/department/DepartmentPage';
-import Notice from './ErrrorPages/UnderProcess/UnderProgress';
+import Notice from './pages/notice/NoticePage';
 import Events from './ErrrorPages/UnderProcess/UnderProgress';
 import Gallery from './ErrrorPages/UnderProcess/UnderProgress';
 import AdminLogin from './components/adminInterface/pages/login/Login';
@@ -15,40 +15,40 @@ const router = createBrowserRouter([
     path: '/login',
     element: <AdminLogin />,
   },
-  
-{
-  path: '/',
+
+  {
+    path: '/',
     element: <HomeBase />,
-      children: [
-        {
-          index: true, // default route for '/'
-          element: <Home />,
-        },
-        {
-          path: 'departments', // '/departments'
-          element: <Departments />,
-        },
-        {
-          path: 'gallery', // '/gallery'
-          element: <Gallery />,
-        },
-        {
-          path: 'notice', // '/notice'
-          element: <Notice />,
-        },
-        {
-          path: 'events', // '/contact'
-          element: <Events />,
-        },
-        {
-          path: 'about', // '/about'
-          element: <About />,
-        },
-        {
-          path: 'contact', // '/contact'
-          element: <Contact />,
-        },
-      ],
+    children: [
+      {
+        index: true, // default route for '/'
+        element: <Home />,
+      },
+      {
+        path: 'departments', // '/departments'
+        element: <Departments />,
+      },
+      {
+        path: 'gallery', // '/gallery'
+        element: <Gallery />,
+      },
+      {
+        path: 'notice', // '/notice'
+        element: <Notice />,
+      },
+      {
+        path: 'events', // '/contact'
+        element: <Events />,
+      },
+      {
+        path: 'about', // '/about'
+        element: <About />,
+      },
+      {
+        path: 'contact', // '/contact'
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
