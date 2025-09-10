85e2e44731fd3b8f586f9d22b88b86c553018215
Kaushal Patil <kaushalpatil.tech@gmail.com>
2025-09-10 18:01:30 +0530

    ok

--- src/constant.js
+++ src/constant.js
@@ -1,10 +1,10 @@
 
 //  For Develpment Mode
-// export const API = "http://localhost:5000/v1/admin";
-// export const BASE_URL = "http://localhost:5000/"
+export const API = "http://localhost:5000/v1/admin";
+export const BASE_URL = "http://localhost:5000/"
 
 // For Production Mode
-export const API = "https://hvpvc-backend.vercel.app/v1/admin";
-export const BASE_URL = "https://hvpvc-backend.vercel.app/";
+// export const API = "https://hvpvc-backend.vercel.app/v1/admin";
+// export const BASE_URL = "https://hvpvc-backend.vercel.app/";
 
 
